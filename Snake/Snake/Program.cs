f2b01f9e0448f43a8fa84631984a42b3a34e390e
siriushard <siriusdev3@gmail.com>
2016-07-16 23:50:03 +0300

    Ram

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,11 +8,26 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+           // Func1(x);
+            Console.WriteLine("Call Func1. x = " + x);
+
+            x = 1;
+           // Func2(x);
+            Console.WriteLine("Call Func2. x = " + x);
+
+            x = 1;
+            //Func3(x);
+            Console.WriteLine("Call Func3. x = " + x);
+
             Point p1 = new Point(1,3,'*');
-            p1.Draw();
+            Console.WriteLine("Call Move. p1.x = " + p1.y);
 
             Point p2 = new Point(4,5,'#');
-            p2.Draw();
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("Call Func1. x = " + x);
 
             Console.ReadLine();
         }
