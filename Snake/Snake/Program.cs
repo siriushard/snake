1262f8ab687be649ee4cb755aaa5685a8093606d
siriushard <siriusdev3@gmail.com>
2016-07-16 22:54:15 +0300

    Hello World

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
